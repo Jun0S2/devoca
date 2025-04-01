c40035a8b3077a9a17f4c4cd158e764b1d609d7d
June Park <leen6225@gmail.com>
2025-04-01 18:25:52 +0900

    [FE][Styel] Add Logo Icon

--- frontend/app/utils/navbar.tsx
+++ frontend/app/utils/navbar.tsx
@@ -1,5 +1,6 @@
 // app/components/navbar.tsx
 import { Link, useLocation } from "@remix-run/react";
+import logo from "public/logo.png"
 
 export function Navbar() {
   const location = useLocation();
@@ -10,10 +11,18 @@ export function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white shadow-sm z-50">
       <div className="max-w-7xl mx-auto px-4 h-14 flex items-center justify-between">
-        {/* Home */}
-        <Link to="/" className="flex items-center text-gray-700 hover:text-red-200">
+          {/* Home (로고 이미지) */}
+          <Link to="/" className="flex items-center">
+          <img
+            src={logo}
+            alt="DEVoca Logo"
+            className="h-8 w-auto object-contain" // height 조절: h-8 = 2rem
+          />
+          <div className="flex items-center text-gray-700 hover:text-red-200">
+          </div>
           <span className="text-xl font-bold text-red-400">DEVoca</span>
         </Link>
+
 
         {/* Level 메뉴 */}
         <div className="flex items-center space-x-2 md:space-x-4">
